402210004f5246ab0669cbfb42ba5e6064df0cfa
Christian <chrisdb150@gmail.com>
2024-08-06 12:42:32 -0400

    theming parte 1

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,22 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from "@angular/common/http";
+import { ApplicationConfig } from "@angular/core";
+import { provideAnimationsAsync } from "@angular/platform-browser/animations/async";
+import { provideRouter } from "@angular/router";
+import { authInterceptor } from "@core/interceptors/auth.interceptor";
+import { TokenService } from "@core/services/token.service";
+import { AuthService } from "@authentication/services/auth.service";
 
-import { routes } from './app.routes';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+import { routes } from "./app.routes";
+import { CookieService } from "ngx-cookie-service";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+    providers: [
+        provideAnimationsAsync(),
+        provideRouter(routes),
+        provideHttpClient(withInterceptors([authInterceptor])),
+        AuthService,
+        TokenService,
+        CookieService,
+    ]
 };
