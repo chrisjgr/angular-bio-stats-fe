4ea69d9a33bfe2f76862cecacb26a07c8ca41dfc
Christian <chrisdb150@gmail.com>
2024-08-12 21:48:01 -0400

    adding new backend service

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@
 
 export const environment = {
   apiSecret: 'jwt-bio-stats-tech-test-api-secret',
-  apiUrl: 'https://nest-bio-stats-be.zeabur.app/api',
+  apiUrl: 'https://nest-bio-stats-be.vercel.app/api',
   jwtSecret: '',
   production: false,
 };
